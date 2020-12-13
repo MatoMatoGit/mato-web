5acdb1370c7ee4ca31177772105e7c8751e3e226
anraijmakers <lex.raijmakers@proudnerds.com>
2020-12-13 14:34:40 +0100

    fix: Add build

---
+++ build/precache-manifest.7eb8e211022cd9449db72efc5e4e8836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "698790b6d53db6946dd0",
+    "url": "/static/css/main.8cb48e7f.chunk.css"
+  },
+  {
+    "revision": "698790b6d53db6946dd0",
+    "url": "/static/js/main.01ebec00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e8e03d1612777cd62750",
+    "url": "/static/css/2.3fb4637a.chunk.css"
+  },
+  {
+    "revision": "e8e03d1612777cd62750",
+    "url": "/static/js/2.5190ac60.chunk.js"
+  },
+  {
+    "revision": "62e55d7a186576bad475005c0b0600c5",
+    "url": "/index.html"
+  }
+];
